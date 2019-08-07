c40384543c23eddee23831f586d4d728caabe940
brtkozak <brtkozak@gmail.com>
2019-08-07 21:34:36 +0200

    com

--- app/src/main/java/com/google/ticketo/ui/profile/MyProfileView.kt
+++ app/src/main/java/com/google/ticketo/ui/profile/MyProfileView.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 import com.google.ticketo.R
+import kotlinx.android.synthetic.main.my_profile_fragment.*
 
 class MyProfileView : Fragment() {
 
@@ -30,6 +33,12 @@ class MyProfileView : Fragment() {
         // TODO: Use the ViewModel
 
 
+        Glide.with(this)
+            .load("https://graph.facebook.com/2864734686933587/picture?height=300&width=300")
+            .apply(RequestOptions.circleCropTransform())
+            .placeholder(R.drawable.ic_launcher_background)
+            .override(350, 350)
+            .into(my_profile_profile_pic)
 
     }
 
